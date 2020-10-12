82e7c9e37b6db289523832fc77e56354ded1cdde
Ashish Jha <ashish.jha@revolut.com>
2020-10-12 11:09:03 +0530

    adding permute to cpp code in chapter 10

--- 10_operationalizing_pytorch_models_into_production/cpp_convnet/cpp_convnet.cpp
+++ 10_operationalizing_pytorch_models_into_production/cpp_convnet/cpp_convnet.cpp
@@ -12,12 +12,13 @@ int main(int argc, char **argv) {
     Mat img = imread(argv[2], IMREAD_GRAYSCALE);
     resize(img, img, Size(28, 28));
     auto input_ = torch::from_blob(img.data, { img.rows, img.cols, img.channels() }, at::kByte);
-    auto input = input_.unsqueeze_(0).reshape({1, 1, img.rows, img.cols}).toType(c10::kFloat).div(255);
+    auto input = input_.permute({2,0,1}).unsqueeze_(0).reshape({1, 1, img.rows, img.cols}).toType(c10::kFloat).div(255);
     input = (input - 0.1302) / 0.3069;
     auto module = torch::jit::load(argv[1]);
     std::vector<torch::jit::IValue> inputs;
     inputs.push_back(input);
     auto output_ = module.forward(inputs).toTensor();
+    cout << output_ << '\n';
     auto output = output_.argmax(1);
     cout << output << '\n';
     return 0;
